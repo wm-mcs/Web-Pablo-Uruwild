b53fa0f670cebd2bd12fe85ce63d9b3b60df7de0
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-29 13:22:46 -0300

    df

--- app/Http/Rutas/Cabañas/Rutas_cabañas.php
+++ app/Http/Rutas/Cabañas/Rutas_cabañas.php
@@ -2,30 +2,30 @@
 
 Route::get('get_admin_cabañas',
 [
-  'uses'  => 'Admin_Empresa\Admin_Team_Controllers@get_admin_cabañas',
+  'uses'  => 'Admin_Empresa\Admin_Cabaña_Controllers@get_admin_cabañas',
   'as'    => 'get_admin_cabañas'
 ]);
 
 Route::get('get_admin_cabañas_crear',
 [
-  'uses'  => 'Admin_Empresa\Admin_Team_Controllers@get_admin_cabañas_crear',
+  'uses'  => 'Admin_Empresa\Admin_Cabaña_Controllers@get_admin_cabañas_crear',
   'as'    => 'get_admin_cabañas_crear'
 ]);
 
 Route::post('set_admin_cabañas_crear',
 [
-  'uses'  => 'Admin_Empresa\Admin_Team_Controllers@set_admin_cabañas_crear',
+  'uses'  => 'Admin_Empresa\Admin_Cabaña_Controllers@set_admin_cabañas_crear',
   'as'    => 'set_admin_cabañas_crear'
 ]);
 
 Route::get('get_admin_cabañas_editar{id}',
 [
-  'uses'  => 'Admin_Empresa\Admin_Team_Controllers@get_admin_cabañas_editar',
+  'uses'  => 'Admin_Empresa\Admin_Cabaña_Controllers@get_admin_cabañas_editar',
   'as'    => 'get_admin_cabañas_editar'
 ]);
 
 Route::patch('set_admin_cabañas_editar{id}',
 [
-  'uses'  => 'Admin_Empresa\Admin_Team_Controllers@set_admin_cabañas_editar',
+  'uses'  => 'Admin_Empresa\Admin_Cabaña_Controllers@set_admin_cabañas_editar',
   'as'    => 'set_admin_cabañas_editar'
 ]);
