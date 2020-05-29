cd2d1d6fef28519cb587ddbed65c184baaccfdab
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-29 13:31:56 -0300

    gh

--- app/Entidades/Cabaña.php
+++ app/Entidades/Cabaña.php
@@ -19,7 +19,18 @@ class Cabaña extends Model
      */
     protected $fillable = ['name'];
 
-    
+    public function scopeName($query, $name)
+    {
+        //si el paramatre(campo busqueda) esta vacio ejecutamos el codigo
+        /// trim() se utiliza para eliminar los espacios.
+        ////Like se usa para busqueda incompletas
+        /////%% es para los espacios adelante y atras
+        if (trim($name) !="")
+        {                        
+           $query->where('name', "LIKE","%$name%"); 
+        }
+        
+    }
    
     
 }
