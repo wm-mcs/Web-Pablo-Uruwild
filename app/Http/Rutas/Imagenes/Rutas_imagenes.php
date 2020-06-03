72c0967fdf7e2c4117276eddb7e1fd196ea594d3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-03 12:05:48 -0300

    fg

--- app/Http/Rutas/Imagenes/Rutas_imagenes.php
+++ app/Http/Rutas/Imagenes/Rutas_imagenes.php
@@ -1,12 +1,12 @@
 <?php 
 
-Route::get('borrar_esta_imagen{id}{nombre_campo}',
+Route::get('borrar_esta_imagen{id}',
 [
   'uses'  => 'Admin_Empresa\Admin_Imagen_Controllers@borrar_esta_imagen',
   'as'    => 'borrar_esta_imagen'
 ]);
 
-Route::get('establecer_como_principal{id}-{nombre_campo}',
+Route::get('establecer_como_principal{id}',
 [
   'uses'  => 'Admin_Empresa\Admin_Imagen_Controllers@establecer_como_principal',
   'as'    => 'establecer_como_principal'
