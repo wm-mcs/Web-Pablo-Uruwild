6fa966b0d8759e6f4cb94a35a7cb5e6e894316e0
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-02 22:03:00 -0300

    df

--- resources/views/admin/team/partes/lista.blade.php
+++ resources/views/admin/team/partes/lista.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-6 col-lg-4 mb-4">
-    <div class="servicio_lista service d-flex flex-column align-items-center">
+    <div class="servicio_lista service d-flex flex-column align-items-center p-5">
       
       <div class="col-6 mb-5 d-flex flex-row justify-content-center align-items-center ">
         <img src="{{$Entidad->url_img_foto_principal_chica}}" alt="{{$Entidad->descripcion_breve}}" class="img-fluid rounded-circle">
@@ -7,7 +7,7 @@
         
                    
       <div class="p-3 mt-2">
-        <h3 class="sub-titulos-class   mb-2">
+        <h3 class="sub-titulos-class text-center   mb-2">
           <a href="{{$Route}}" class="font-primary text-center text-color-secondary">
            {{$Entidad->first_name}}
           </a>                
@@ -21,27 +21,27 @@
         <div class="row col-12 d-flex flex-row justify-content-center align-items-center">
           <div class="d-flex flex-row align-items-center">
             @if(!$Entidad->facebook_valor) 
-            <a class="p-3 col-2 d-block team-social-personalizado" href="{{$Entidad->facebook_valor}}">
+            <a class="p-3 d-block team-social-personalizado" href="{{$Entidad->facebook_valor}}">
               <i class="fab fa-facebook-f"></i>
             </a>
             @endif
             @if(!$Entidad->instagram_valor) 
-            <a class="p-3 col-2 d-block team-social-personalizado" href="{{$Entidad->instagram_valor}}">
+            <a class="p-3  d-block team-social-personalizado" href="{{$Entidad->instagram_valor}}">
               <i class="fab fa-instagram"></i>
             </a>
             @endif
             @if(!$Entidad->youtube_valor) 
-            <a class="p-3 col-2 d-block team-social-personalizado" href="{{$Entidad->youtube_valor}}">
+            <a class="p-3  d-block team-social-personalizado" href="{{$Entidad->youtube_valor}}">
               <i class="fab fa-youtube"></i>
             </a>
             @endif
             @if(!$Entidad->linkedin_valor) 
-            <a class="p-3 col-2 d-block team-social-personalizado" href="{{$Entidad->linkedin_valor}}">
+            <a class="p-3  d-block team-social-personalizado" href="{{$Entidad->linkedin_valor}}">
               <i class="fab fa-linkedin-in"></i>
             </a>
             @endif
             @if(!$Entidad->whatsapp_valor) 
-            <a class="p-3 col-2 d-block team-social-personalizado" href="{{$Entidad->whatsapp_valor}}">
+            <a class="p-3  d-block team-social-personalizado" href="{{$Entidad->whatsapp_valor}}">
               <i class="fab fa-whatsapp"></i>
             </a>
             @endif
