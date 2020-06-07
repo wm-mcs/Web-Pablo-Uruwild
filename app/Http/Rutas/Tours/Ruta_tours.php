6a4fd6239c93c0e21992d926fb327170eab8a8af
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-07 19:11:24 -0300

    df

--- app/Http/Rutas/Tours/Ruta_tours.php
+++ app/Http/Rutas/Tours/Ruta_tours.php
@@ -6,26 +6,26 @@ Route::get('get_admin_tours',
   'as'    => 'get_admin_tours'
 ]);
 
-Route::get('get_admin_tours',
+Route::get('get_admin_tours_crear',
 [
   'uses'  => 'Admin_Empresa\Admin_Tours_Controllers@get_admin_crear',
-  'as'    => 'get_admin_tours'
+  'as'    => 'get_admin_tours_crear'
 ]);
 
-Route::post('set_admin_tours',
+Route::post('set_admin_tours_crear',
 [
   'uses'  => 'Admin_Empresa\Admin_Tours_Controllers@set_admin_crear',
-  'as'    => 'set_admin_tours'
+  'as'    => 'set_admin_tours_crear'
 ]);
 
-Route::get('get_admin_tours{id}',
+Route::get('get_admin_tours_editar{id}',
 [
   'uses'  => 'Admin_Empresa\Admin_Tours_Controllers@get_admin_editar',
-  'as'    => 'get_admin_tours'
+  'as'    => 'get_admin_tours_editar'
 ]);
 
-Route::patch('set_admin_tours{id}',
+Route::patch('set_admin_tours_editar{id}',
 [
   'uses'  => 'Admin_Empresa\Admin_Tours_Controllers@set_admin_editar',
-  'as'    => 'set_admin_tours'
+  'as'    => 'set_admin_tours_editar'
 ]);
