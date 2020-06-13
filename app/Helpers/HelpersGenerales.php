d4134e323313a6a2b4e38f6f81194395ad479bd3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-13 20:43:15 -0300

    ok

--- app/Helpers/HelpersGenerales.php
+++ app/Helpers/HelpersGenerales.php
@@ -112,7 +112,7 @@ class HelpersGenerales
 
         
 
-        return htmlentities($cadena, ENT_QUOTES | ENT_IGNORE, "UTF-8"); 
+        /*return htmlentities($cadena, ENT_QUOTES | ENT_IGNORE, "UTF-8");*/ 
         return $cadena; 
     }
 
