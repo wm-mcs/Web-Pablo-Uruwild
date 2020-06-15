9eed0960391d885c6cafb87b36ac19f710f034bd
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-14 23:01:34 -0300

    df

--- resources/views/layouts/paginasPersonalizadas/layout.blade.php
+++ resources/views/layouts/paginasPersonalizadas/layout.blade.php
@@ -62,9 +62,11 @@
        <script  src="https://unpkg.com/vue@2.5.17/dist/vue.min.js"></script> 
       @endif
   @endif
-  <script  src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script> 
+  {{-- <script  src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script> --}} 
+
   {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-select/2.6.2/vue-select.js"></script> --}}
-  <script  src="https://unpkg.com/lodash@4.13.1/lodash.min.js"></script>
+  
+  {{-- <script  src="https://unpkg.com/lodash@4.13.1/lodash.min.js"></script> --}}
 
   <script type="text/javascript">
     @yield('vue')  
