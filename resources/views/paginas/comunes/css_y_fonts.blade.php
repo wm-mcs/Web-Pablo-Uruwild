6dc9b79d9884843e16cf79b29dae4d398e96d4c2
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-14 23:21:40 -0300

    df

--- resources/views/paginas/comunes/css_y_fonts.blade.php
+++ resources/views/paginas/comunes/css_y_fonts.blade.php
@@ -4,4 +4,4 @@
     <link rel="stylesheet" href="{{url()}}/fonts/flaticon/font/flaticon.css">  
 
     <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/credo.css') }}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8cca488c92.js" crossorigin="anonymous"></script>
