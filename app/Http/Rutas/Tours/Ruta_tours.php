4de3aebcbc63c673eaa0233522a6d3dbd70f4151
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-23 13:15:02 -0300

    fg

--- app/Http/Rutas/Tours/Ruta_tours.php
+++ app/Http/Rutas/Tours/Ruta_tours.php
@@ -13,17 +13,17 @@ Route::get('get_admin_tours',
         'as'    => 'get_admin_productos_especiales'
       ]);
 
-Route::get('get_admin_productos_especiales_crear',
+Route::get('get_admin_tours_crear',
 [
   'uses'  => 'Admin_Empresa\Admin_Tours_Controllers@get_admin_crear',
-  'as'    => 'get_admin_productos_especiales_crear'
+  'as'    => 'get_admin_tours_crear'
 ]);
 
        // R u t a   d e   l o s   p r o d u c t o s   e s p e c i a l e s 
-      Route::get('get_admin_productos_especiales',
+      Route::get('get_admin_productos_especiales_crear',
       [
         'uses'  => 'Admin_Empresa\Admin_Producto_Especial_Controllers@get_admin_crear',
-        'as'    => 'get_admin_productos_especiales'
+        'as'    => 'get_admin_productos_especiales_crear'
       ]);
 
 Route::post('set_admin_tours_crear',
