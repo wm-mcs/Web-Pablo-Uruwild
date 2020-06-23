957e861cd68aef9d3a1de7d4fec36e9a33678068
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-23 13:29:37 -0300

    fg

--- app/Repositorios/TourRepo.php
+++ app/Repositorios/TourRepo.php
@@ -15,21 +15,21 @@ class TourRepo extends BaseRepo
 
 
   public function getEntidadesParaHome($Cantidad,$Order_key, $Order_value = 'desc',$tipo_de_tour)
-    {
-     $Entidades = $this->getEntidad()
-                       ->where('tipo_de_tour',$tipo_de_tour)
-                       ->where('estado','si')
-                       ->where('borrado','no')
-                       ->orderBy($Order_key,$Order_value)
-                       ->get();
+  {
+   $Entidades = $this->getEntidad()
+                     ->where('tipo_de_tour',$tipo_de_tour)
+                     ->where('estado','si')
+                     ->where('borrado','no')
+                     ->orderBy($Order_key,$Order_value)
+                     ->get();
 
-     if($Entidades->count() >= $Cantidad )   
-     {
-      return $Entidades->take($Cantidad);
-     }  
+   if($Entidades->count() >= $Cantidad )   
+   {
+    return $Entidades->take($Cantidad);
+   }  
 
-      return   $Entidades;           
-    }
+    return   $Entidades;           
+  }
 
 
   public function getEntidadesDeToursPaginadas($request,$paginada,$tipo_de_tour,$orderBy = 'id', $vañor = 'desc' )
