a937e7b13c3fb1069c7a362df6014c9f86f50717
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-01 20:05:46 -0300

    df

--- resources/views/admin/tours/editar.blade.php
+++ resources/views/admin/tours/editar.blade.php
@@ -2,51 +2,39 @@
 
 
 @section('miga-de-pan')
- 
-
  <h1 class="titulos-class mb-3  text-color-secondary font-secondary">Editar</h1> 
- <p class="parrafo-class color-text-gris mb-3"> Link pública <i class="fas fa-hand-point-right"></i> <a href="{{$Entidad->route}}"  target="_blank">{{$Entidad->name}}</a></p>
-
-  <p class="parrafo-class color-text-gris"> Para guardar los cambios deben apretar el botón que está abajo del todo</p>
-  
+ <p class="parrafo-class color-text-gris mb-3"> 
+  Link pública <i class="fas fa-hand-point-right"></i> 
+  <a href="{{$Entidad->route}}"  target="_blank">{{$Entidad->name}}</a>
+ </p>
+ <p class="parrafo-class color-text-gris"> 
+  Para guardar los cambios deben apretar el botón que está abajo del todo
+ </p>
 @stop
 
 @section('content')
-
-
-
- {{-- formulario --}}
+  {{-- formulario --}}
   {!! Form::model($Entidad,   ['route' => [$Route_editar_post,$Entidad->id],
-                            'method'=> 'patch',
-                            'files' =>  true,
-                            'id'    => 'form-admin-empresa-datos'
+                               'method'=> 'patch',
+                               'files' =>  true,
+                               'id'    => 'form-admin-empresa-datos'
                           ])               !!}
    <div class="row p-5">
-
-
-       {{--
-       P a r a   q u e   s e   c r e e o   t i p o   p r o d u c t o .   T e n e r   e n   c u e n t a   q u e   e n   b a s e   d e   d a t o s   s e   c r e a  c o m o    t o u r   p o r   d e f e c t o  
-      --}}
-      {{-- {!! Form::hidden('tipo_de_tour') !!} --}}
-
-      {{-- datos corporativos --}}
-      <div class="contenedor-grupo-datos col-12 col-lg-6">
+    {{-- datos corporativos --}}
+    <div class="contenedor-grupo-datos col-12 col-lg-6">
         <div class="contenedor-grupo-datos-titulo"> Datos</div>
         <div class="contenedor-formulario-label-fiel">                       
           @include('admin.'.$Carpeta_view_admin.'.formularios_partes.datos_basicos')
         </div>
-      </div>
-
-
-      {{-- imagenes corporativos --}}
-      <div class="contenedor-grupo-datos col-12 col-lg-6">
+    </div>
+    {{-- imagenes corporativos --}}
+    <div class="contenedor-grupo-datos col-12 col-lg-6">
         <div class="contenedor-grupo-datos-titulo"> Imagen</div>
         <div class="contenedor-formulario-label-fiel">                       
           @include('admin.'.$Carpeta_view_admin.'.formularios_partes.datos_imagenes')
         </div>
-      </div>
-
-       <div class="contenedor-grupo-datos col-12">
+    </div>
+    <div class="contenedor-grupo-datos col-12">
         <div class="contenedor-grupo-datos-titulo">Contenido </div>
         <div class="contenedor-formulario-label-fiel">                       
           <div class="formulario-label-fiel">
@@ -56,25 +44,10 @@
                                                      'rows' => 20 ]) !!}
           </div>
         </div>
-      </div>
-     
-      
-
-      
-
-     
-   </div>
-   
-    <div class="mt-5 mb-5 Boton-Fuente-Chica Boton-Secondary-Relleno disparar-este-form">
-     Guardar cambios   <i class="fas fa-angle-double-right"></i>
-    </div> 
-
-      
-
-      
-   
-   
-
+    </div>     
+  </div>   
+  <div class="mt-5 mb-5 Boton-Fuente-Chica Boton-Secondary-Relleno disparar-este-form">
+      Guardar cambios   <i class="fas fa-angle-double-right"></i>
+  </div> 
   {!! Form::close() !!}
-
 @stop
