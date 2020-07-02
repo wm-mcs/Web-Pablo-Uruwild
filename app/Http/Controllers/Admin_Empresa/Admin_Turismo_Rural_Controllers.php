0931cb45cc4a00b1db3f6b381225e1134c105405
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-02 14:44:35 -0300

    df

---
+++ app/Http/Controllers/Admin_Empresa/Admin_Turismo_Rural_Controllers.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Admin_Empresa;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Repositorios\TourRepo;
+use App\Repositorios\ImagenRepo;
+use App\Helpers\HelpersGenerales;
+use App\Managers\tour_manager;
+use App\Interfaces\entidadCrudControllerInterface;
+use App\Traits\entidadesControllerComunesCrud;
+
+
+
+
+class Admin_Turismo_Rural_Controllers extends Controller implements entidadCrudControllerInterface
+{
+
+
+  use entidadesControllerComunesCrud;
+
+  protected $Entidad_principal;
+  protected $Nombre_entidad_plural      ;
+  protected $Nombre_entidad_singular    ;
+  protected $Carpeta_view_admin         ; 
+  protected $Path_view_get_admin_index  ;
+  protected $Path_view_get_admin_crear  ;
+  protected $Path_view_get_admin_editar ;
+  protected $Route_index ;               
+  protected $Route_crear  ;             
+  protected $Route_crear_post ;          
+  protected $Route_editar_post ;         
+  protected $Route_luego_de_crear;       
+  protected $Path_carpeta_imagenes;      
+  protected $Nombre_del_campo_imagen;    
+
+  
+
+  public function __construct(TourRepo   $TourRepo,
+                              ImagenRepo $ImagenRepo )
+  {
+    $this->Entidad_principal          = $TourRepo;
+    $this->ImagenRepo                 = $ImagenRepo;
+    $this->Nombre_entidad_plural      = 'Tours';
+    $this->Nombre_entidad_singular    = 'Tour';
+    $this->Carpeta_view_admin         = strtolower(str_replace(' ','_', $this->Nombre_entidad_plural));
+    $this->Path_view_get_admin_index  = 'admin.turismo_rural.home';
+    $this->Path_view_get_admin_crear  = 'admin.turismo_rural.crear';
+    $this->Path_view_get_admin_editar = 'admin.' . $this->Carpeta_view_admin . '.editar';
+    $this->Route_index                = 'get_admin_turismo_rural';
+    $this->Route_crear                = 'get_admin_turismo_rural_crear';
+    $this->Route_crear_post           = 'set_admin_'. $this->Carpeta_view_admin .'_crear';
+    $this->Route_editar_post          = 'set_admin_'. $this->Carpeta_view_admin .'_editar';
+    $this->Route_luego_de_crear       = $this->Route_index;
+    $this->Path_carpeta_imagenes      = $this->Carpeta_view_admin .'/'; //donde se gurarda la imagen. Debe existir
+    $this->Nombre_del_campo_imagen    = strtolower($this->Nombre_entidad_singular) . '_id';    
+  }
+
+  public function getPropiedades()
+  {
+    return ['name','fecha_inicio','cantidad_de_dias','description','descripcion_breve','precio','estado','tipo_de_tour','rank'];
+  }
+
+  public function getManager($Request)
+  {
+    $manager = new tour_manager(null, $Request->all());
+
+    return $manager;
+  }
+
+  public function getImagenMiniaturaSize()
+  {
+    return 1000;
+  }
+
+  public function olvidarCachesAsociadoAEstaEntidad()
+  {   
+    
+  }
+
+  public function get_admin(Request $Request)
+  { 
+    $Entidades           = $this->Entidad_principal->getEntidadesDeToursPaginadas($Request,9,'turismo_rural','rank','desc');
+    $Titulo              = 'Productos especiales';
+    $Route_crear         = $this->Route_crear;
+    $Route_busqueda      = $this->Route_index;
+    $Carpeta_view_admin  = 'turismo_rural';
+
+
+    return view($this->Path_view_get_admin_index, compact('Entidades','Route_crear','Titulo','Route_busqueda','Carpeta_view_admin'));
+  }
+
+  public function get_admin_crear()
+  {  
+    $Route_crear_post    = $this->Route_crear_post;
+    $Titulo              = 'Crear producto tirismo de turismo';
+    $Carpeta_view_admin  = 'turismo_rural';
+
+    return view($this->Path_view_get_admin_crear,compact('Route_crear_post','Titulo','Carpeta_view_admin'));
+  }
--- app/Http/Rutas/Tours/Ruta_tours.php
+++ app/Http/Rutas/Tours/Ruta_tours.php
@@ -13,17 +13,31 @@ Route::get('get_admin_tours',
         'as'    => 'get_admin_productos_especiales'
       ]);
 
+      // R u t a   d e  t u r i s m o   r u r a l  
+      Route::get('get_admin_turismo_rural',
+      [
+        'uses'  => 'Admin_Empresa\Admin_Turismo_Rural_Controllers@get_admin',
+        'as'    => 'get_admin_turismo_rural'
+      ]);
+
 Route::get('get_admin_tours_crear',
 [
   'uses'  => 'Admin_Empresa\Admin_Tours_Controllers@get_admin_crear',
   'as'    => 'get_admin_tours_crear'
 ]);
 
-       // R u t a   d e   l o s   p r o d u c t o s   e s p e c i a l e s 
+      // R u t a   d e   l o s   p r o d u c t o s   e s p e c i a l e s 
       Route::get('get_admin_productos_especiales_crear',
       [
         'uses'  => 'Admin_Empresa\Admin_Producto_Especial_Controllers@get_admin_crear',
         'as'    => 'get_admin_productos_especiales_crear'
+      ]);
+
+      // R u t a   d e  t u r i s m o   r u r a l 
+      Route::get('get_admin_turismo_rural_crear',
+      [
+        'uses'  => 'Admin_Empresa\Admin_Turismo_Rural_Controllers@get_admin_crear',
+        'as'    => 'get_admin_turismo_rural_crear'
       ]);
 
 Route::post('set_admin_tours_crear',
