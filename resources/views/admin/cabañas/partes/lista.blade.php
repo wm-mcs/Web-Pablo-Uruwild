e1357b41843a6d48947b315dab40aef94448a239
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-03 13:40:18 -0300

    fg

--- resources/views/admin/cabañas/partes/lista.blade.php
+++ resources/views/admin/cabañas/partes/lista.blade.php
@@ -1,6 +1,33 @@
-
-
-
+{{-- E s t a   e s   l a   v i s t a   p a r a   a d m i n  --}}
+@if(isset($Mostrar_admin) && $Mostrar_admin == true)
+<div class="col-md-6 col-lg-4 mb-4">
+    <div class="servicio_lista service">
+      <a href="{{$Route}}">
+        <img data-src="{{$Entidad->url_img_foto_principal_chica}}" alt="{{$Entidad->descripcion_breve}}" class="servicio_lista_imagen">
+      </a>              
+      <div class="p-3 mt-2">
+        <p class="mb-3">
+          @if($Entidad->estado == 'si')
+            <span class="color-text-success">Activo</span>
+          @else
+            <span class="color-text-gris">Inactivo</span>
+          @endif
+        </p>  
+        <h3 class="sub-titulos-class   mb-2">
+          <a href="{{$Route}}" class="font-primary text-color-secondary">
+           {{$Entidad->name}}
+          </a>                
+        </h3>
+        <p class="color-text-gris mb-2 ">
+         {{$Entidad->descripcion_breve}}
+        </p>
+        <p>
+          <a href="{{$Route}}"> Leer más  <i class="fas fa-chevron-right"></i></a>
+        </p>                
+      </div>
+    </div>
+</div>
+@else
 <div class="col-md-6 col-lg-4 mb-4">
     <div class="servicio_lista service">
       <a href="{{$Route}}">
@@ -21,3 +48,4 @@
       </div>
     </div>
 </div>
+@endif
--- resources/views/admin/tours/partes/lista.blade.php
+++ resources/views/admin/tours/partes/lista.blade.php
@@ -2,18 +2,18 @@
 {{-- E s t a   e s   l a   v i s t a   p a r a   a d m i n  --}}
 @if(isset($Mostrar_admin) && $Mostrar_admin == true)
 <div class="col-md-6 col-lg-4 mb-4">
-    <div class="servicio_lista service">
-      <p class="mb-4">
+    <div class="servicio_lista service">      
+      <a href="{{$Route}}">
+        <img src="{{$Entidad->url_img_foto_principal_chica}}" alt="{{$Entidad->descripcion_breve}}" class="servicio_lista_imagen">
+      </a>              
+      <div class="p-3 mt-2">
+        <p class="mb-3">
           @if($Entidad->estado == 'si')
             <span class="color-text-success">Activo</span>
           @else
             <span class="color-text-gris">Inactivo</span>
           @endif
-      </p>   
-      <a href="{{$Route}}">
-        <img src="{{$Entidad->url_img_foto_principal_chica}}" alt="{{$Entidad->descripcion_breve}}" class="servicio_lista_imagen">
-      </a>              
-      <div class="p-3 mt-2">
+        </p>   
         <h3 class="sub-titulos-class   mb-2">
           <a href="{{$Route}}" class="font-primary text-color-secondary">
            {{$Entidad->name}}
