45a77e04642aac0e834d49b4ddbfbbf075699f7a
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-05 19:48:03 -0300

    df

--- resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
+++ resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
@@ -111,11 +111,12 @@
       <p class="m-0 text-center color-text-white parrafo-class"> 
         <small>
          <span class="color-text-white">Todos los drechos reservados @{{empresa.name}} © 2020</span> 
-          <span class="text-color-primary"> | </span>
+
+          <span v-if="mostrar_para_grande" class="text-color-primary"> | </span>
+          <br v-else>
 
          <i class="fas fa-code"></i> <span class="color-text-gris">Desarrollado por</span>   
          <a class="color-text-white" href="https://mauricio.mwebs.com.uy/"> Mauricio Costanzo</a>
-        </small>
-      
+        </small>      
       </p>
     </div>
