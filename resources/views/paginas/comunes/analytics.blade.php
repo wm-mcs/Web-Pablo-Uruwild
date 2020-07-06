5b0b529c472c9d463c84cfa478bd76e3a95eaca4
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-06 12:55:42 -0300

    df

--- resources/views/paginas/comunes/analytics.blade.php
+++ resources/views/paginas/comunes/analytics.blade.php
@@ -1,1 +1,10 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-78733215-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-78733215-4');
+</script>
 
