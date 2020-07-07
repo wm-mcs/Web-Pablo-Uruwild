0f5036ba3e50832d358ef437ac3a59aa86aae6cb
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-07 11:27:12 -0300

    df

--- resources/views/paginas/paginas_personalizadas/partials/portada_molde.blade.php
+++ resources/views/paginas/paginas_personalizadas/partials/portada_molde.blade.php
@@ -1,7 +1,6 @@
-{{--  N u e v o   d i s e ñ o  --}}
+{{-- Se necesita asignar la variable $Portada y $Route. Si $Route no se quiere definir se le dará valor "" (string vacio) y toamara el link que viene del objeto portada y se usara scrool_to en el llamado a la acción --}}
 
-{{--*/  $Portada   =  $Portada_rural /*--}}
-<div v-if="scrolled" class="site-blocks-cover overlay bg-light" id="home-section">
+<div v-if="scrolled" class="site-blocks-cover overlay bg-light" id="circuitos">
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-12 mt-lg-5 text-left align-self-center text-intro">
@@ -15,9 +14,16 @@
             @if($Portada->parrafo != '')
             <p class="text-white no-mostrar-en-mobil mb-3">{{$Portada->parrafo}}
             </p>
-            @endif  
-       
-            <p class="mt-3"><a href="{{route('get_pagina_turismo_rural')}}" class=" Boton-Fuente-Chico Boton-Blanco"> {{$Portada->llamado_a_la_accion}} <i class="fas fa-chevron-right"></i> </a></p>
+            @endif 
+            @if($Route == '')
+            {
+               <p class="mt-3"><a href="{{$Portada->link_llamado_a_la_accion}}" class="scroll_to Boton-Fuente-Chico Boton-Blanco"> {{$Portada->llamado_a_la_accion}} <i class="fas fa-chevron-right"></i> </a></p>
+            }  
+            @else
+            {
+              <p class="mt-3"><a href="{{$Route}}" class=" Boton-Fuente-Chico Boton-Blanco"> {{$Portada->llamado_a_la_accion}} <i class="fas fa-chevron-right"></i> </a></p>
+            }     
+            
           </div>
         </div>
       </div>
@@ -25,10 +31,5 @@
   </div>
   
   <img v-if="mostrar_para_celuar" class="imagen-portada-altura-100vh" style="position: absolute;top: 0;" src="{{$Portada->url_img_foto_principal_chica}}" alt="{{$Portada->titulo}} -{{$Portada->sub_titulo}} -  {{$Portada->parrafo}}  Uruwild.">
-  <img v-else class="imagen-portada-altura-100vh" style="position: absolute;top: 0;" src="{{$Portada->url_img_foto_principal}}" alt="{{$Portada->titulo}} - {{$Portada->sub_titulo}} -  {{$Portada->parrafo}} Uruwild.">
-   
-
-</div>     
-
-
-
+  <img v-else class="imagen-portada-altura-100vh" style="position: absolute;top: 0;" src="{{$Portada->url_img_foto_principal}}" alt="{{$Portada->titulo}} - {{$Portada->sub_titulo}} -  {{$Portada->parrafo}} Uruwild.">  
+</div> 
