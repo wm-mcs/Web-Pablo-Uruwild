dd17ca4421a6d0e82f3edb7e54e6f6f3f0420d45
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-07 13:52:12 -0300

    df

--- resources/views/paginas/paginas_personalizadas/partials/portada_molde.blade.php
+++ resources/views/paginas/paginas_personalizadas/partials/portada_molde.blade.php
@@ -6,15 +6,27 @@
       <div class="col-md-12 mt-lg-5 text-left align-self-center text-intro">
         <div class="row @if($Portada->posicion == 'left') @elseif($Portada->posicion == 'center') justify-content-center text-center @else justify-content-end text-right @endif">
           <div class="col-lg-8 " style="max-width: 550px;">
-            <h1  class="titulos-class text-white font-secondary mb-3">{{$Portada->titulo}}</h1>
-            @if($Portada->sub_titulo != '')
-            <h2 class="sub-titulos-class text-white no-mostrar-en-mobil mb-3">{{$Portada->sub_titulo}}
-            </h2>
-            @endif
-            @if($Portada->parrafo != '')
-            <p class="text-white no-mostrar-en-mobil mb-3">{{$Portada->parrafo}}
-            </p>
-            @endif 
+            @if(isset($EsPortada)) 
+              <h1  class="titulos-class text-white font-secondary mb-3">{{$Portada->titulo}}</h1>
+              @if($Portada->sub_titulo != '')
+              <h2 class="sub-titulos-class text-white no-mostrar-en-mobil mb-3">{{$Portada->sub_titulo}}
+              </h2>
+              @endif
+              @if($Portada->parrafo != '')
+              <p class="text-white no-mostrar-en-mobil mb-3">{{$Portada->parrafo}}
+              </p>
+              @endif 
+            @else
+              <h2  class="titulos-class text-white font-secondary mb-3">{{$Portada->titulo}}</h2>
+              @if($Portada->sub_titulo != '')
+              <h3 class="sub-titulos-class text-white no-mostrar-en-mobil mb-3">{{$Portada->sub_titulo}}
+              </h3>
+              @endif
+              @if($Portada->parrafo != '')
+              <p class="text-white no-mostrar-en-mobil mb-3">{{$Portada->parrafo}}
+              </p>
+              @endif 
+            @endif           
             @if($Route == '')
                <p class="mt-3"><a href="{{$Portada->link_llamado_a_la_accion}}" class="scroll_to Boton-Fuente-Chico Boton-Blanco"> {{$Portada->llamado_a_la_accion}} <i class="fas fa-chevron-right"></i> </a></p>
             @else            
