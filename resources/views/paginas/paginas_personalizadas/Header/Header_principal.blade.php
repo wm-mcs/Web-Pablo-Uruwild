745295df928a1c965b686f502ce07547c04f5175
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-27 11:08:36 -0300

    fg

--- resources/views/paginas/paginas_personalizadas/Header/Header_principal.blade.php
+++ resources/views/paginas/paginas_personalizadas/Header/Header_principal.blade.php
@@ -29,10 +29,11 @@
             <nav class="site-navigation position-relative" role="navigation">
               <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
 
+                <li><a href="{{route('get_home')}}" class="text-uppercase">Inicio</a></li>
                 <li><a href="{{route('get_pagina_tours')}}" class="text-uppercase">Tours de pesca</a></li> 
                 <li><a href="{{route('get_pagina_productos')}}" class="text-uppercase">ecoturismo</a></li>
                 <li><a href="{{route('get_pagina_turismo_rural')}}" class="text-uppercase">turismo rural</a></li>
-                <li><a href="{{route('get_pagina_cabañas')}}" class="text-uppercase">Estancias</a></li>
+                {{-- <li><a href="{{route('get_pagina_cabañas')}}" class="text-uppercase">Estancias</a></li> --}}
                 <li><a href="{{route('get_pagina_quien_es')}}" class="text-uppercase">Sobre Uruwild</a></li>
                 <li><a href="{{route('get_pagina_contacto')}}" class="text-uppercase">Contacto</a></li>
                
