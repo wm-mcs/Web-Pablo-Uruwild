15d6b1982b64e63cce8ff5a9c370ab8a39488624
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-08-24 12:28:24 -0300

    fg

--- app/Http/Middleware/Lenguaje.php
+++ app/Http/Middleware/Lenguaje.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+
 use Closure;
 
 
@@ -8,7 +12,27 @@ class Lenguaje
 {
     public function handle($Request, Closure $next)
     {
-        
+        if(Auth::guest() || Auth::user()->first_name == 'Mauricio')
+        {
+          // Si la sesión no existe hacer lo siguiente
+          if(!Session::has('lenguaje'))
+          {
+            // I p   d e l   u s u a r i o
+            $ip_del_user  = strval($_SERVER['REMOTE_ADDR']); 
+            $data_user    = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+            dd($data_user);
+
+          }
+
+          // Si la sesión existe retornar
+
+
+
+
+
+          return $next($request);
+        }
 
 
         return $next($request);
