5be511a62ded82262081ca9a3db3ee1174a1fc1a
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-08-26 14:19:55 -0300

    hj

--- app/Http/Middleware/Lenguaje.php
+++ app/Http/Middleware/Lenguaje.php
@@ -33,6 +33,8 @@ class Lenguaje
             //se debe verificar el parametro que viene desde la ruta para saber cual es y se lo compara con los idiomas instalados            
             HelpersSessionLenguaje::getAndPutSessionLenguaje(null,$Request->route('lenguaje'));
 
+            dd(ession::get('lenguaje'))
+
             /*
               First Method
 
