2e0e3365b1d458a350d52c05bd40c1df8f747c19
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-09-25 14:20:46 -0300

    fg

--- resources/views/paginas/paginas_personalizadas/partials/textos.blade.php
+++ resources/views/paginas/paginas_personalizadas/partials/textos.blade.php
@@ -1,7 +1,6 @@
 
-  @if($Textos->count() > 0)
-  {{dd($Textos->where('name',$Key)->first())}}
+  @if($Textos->count() > 0)  
     @if(isset($Textos->where('name',$Key)->name_formateado_con_lenguaje))
-      {{$Textos->where('name',$Key)->name_formateado_con_lenguaje}}
+      {{$Textos->where('name',$Key)->first()->name_formateado_con_lenguaje}}
     @endif
   @endif
