506d5dea4397fecef480845fdf9d5a905cc3ee64
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-09-25 14:13:25 -0300

    fg

--- config/paginas.php
+++ config/paginas.php
@@ -1,11 +1,11 @@
 <?php 
 
 return [
-'generales', 
-'home',
-'listado_tours',
-'listado_productos',
-'listado_turismo_rural',
-'listado_cabañas',
+'generales'              => 'generales' , 
+'home'                   => 'home' ,
+'listado_tours'          => 'listado_tours' ,
+'listado_productos'      => 'listado_productos' ,
+'listado_turismo_rural'  => 'listado_turismo_rural',
+'listado_cabañas'        => 'listado_cabañas',
 
 ];
