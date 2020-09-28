a3fdd3020a0736548a6f53ff45b23ea366a2c1ab
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-09-28 13:00:42 -0300

    df

--- resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
+++ resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
@@ -24,9 +24,7 @@
              
                <p class="contiene-p-linea">
                  <a class="contiene-link-linea" href="{{route('get_pagina_quien_es',Session::get('lenguaje'))}}" >
-
-                  Sobre Uruwil
-                  {{--*/ $Key  = 'texto sobre uruwild' /*--}}
+                  {{--*/ $Key  = 'nav sobre' /*--}}
                   @include('paginas.paginas_personalizadas.partials.textos')  
 
                  </a>
