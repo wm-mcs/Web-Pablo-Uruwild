4175a39abf91dd5d8a793679f7d3d25a329c4cc5
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-10-09 12:21:05 -0300

    fg

--- resources/views/paginas/paginas_personalizadas/partials/textos.blade.php
+++ resources/views/paginas/paginas_personalizadas/partials/textos.blade.php
@@ -3,7 +3,7 @@
 
     {{--*/ $Entidad_texto  = $Textos->where('name',$Key)->first() /*--}}
     @if(isset($Entidad_texto->name_formateado_con_lenguaje))
-      {{$Entidad_texto->texto_formateado_con_lenguaje}} 
+      {!! $Entidad_texto->texto_formateado_con_lenguaje !!} 
 
        @if(!Auth::guest() )
         @if( Auth::user()->first_name == 'Pablo' || Auth::user()->first_name == 'Mauricio')
