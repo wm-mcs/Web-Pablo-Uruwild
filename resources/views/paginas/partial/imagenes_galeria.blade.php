cb5983332813189629b349b19ae9ded46c0b3b39
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-12-07 13:50:08 -0300

    df

---
+++ resources/views/paginas/partial/imagenes_galeria.blade.php
@@ -0,0 +1,12 @@
+<div class="row justify-content-center mb-5 mx-0">
+    <div class="col-12 col-lg-9 p-1">
+        <div class="row mx-0">
+          @foreach($Entidad->imagenes as $Imagen)
+            <a href="{{$Imagen->url_img}}" data-toggle="lightbox" data-gallery="example-gallery" class="col-6 col-lg-4 p-2 mb-2">
+                <img src="{{$Imagen->url_img_chica}}" class="img-fluid">
+            </a>
+          @endforeach 
+        </div>
+        
+    </div>
+  </div>
--- resources/views/paginas/tours/tour_individual.blade.php
+++ resources/views/paginas/tours/tour_individual.blade.php
@@ -25,6 +25,23 @@
  <meta property="og:image:height"       content="250">
 @stop 
 
+@section('cdn-js')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js" integrity="sha512-Y2IiVZeaBwXG1wSV7f13plqlmFOx8MdjuHyYFVoYzhyRr3nH/NMDjTBSswijzADdNzMyWNetbLMfOpIPl6Cv9g==" crossorigin="anonymous"></script>
+@stop
+
+@section('acciones-js-manuales')
+ <script>
+  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                  event.preventDefault();
+                  $(this).ekkoLightbox();
+              });
+ </script>
+@stop
+
+@section('cdn-css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css" integrity="sha512-Velp0ebMKjcd9RiCoaHhLXkR1sFoCCWXNp6w4zj1hfMifYB5441C+sKeBl/T/Ka6NjBiRfBBQRaQq65ekYz3UQ==" crossorigin="anonymous" />
+@stop
+
 @section('pixcel-facebook')
 
 
