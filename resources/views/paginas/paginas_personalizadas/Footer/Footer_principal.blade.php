9929aa36b4eb16e2d47d2dc5bc24e4e6a1290559
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-12-14 16:32:25 -0300

    Modal del carrito

--- resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
+++ resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
@@ -167,3 +167,31 @@
         </small>      
       </p>
     </div>
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Cambiar de idioma</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+            @foreach(config('lenguajes') as $Lenguaje)
+              
+                <a  href="{{route('get_home_con_lenguaje', $Lenguaje)}}" class="d-block col-6 @if(Session::get('lenguaje') !=  $Lenguaje) p-3 @endif ">
+                    <img class="servicio_lista_imagen" src="{{url()}}/imagenes/Lenguaje/{{$Lenguaje}}.jpg" alt="">
+                </a>
+              
+            @endforeach  
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>                          
+          </div>
+        </div>
+      </div>
+    </div>           
