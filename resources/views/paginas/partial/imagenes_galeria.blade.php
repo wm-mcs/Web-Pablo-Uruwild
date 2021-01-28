d61621bd63e0a1936f9064390250c351ce3d20f2
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-01-28 11:55:02 -0300

    build css e imagenes galeria

--- resources/views/paginas/partial/imagenes_galeria.blade.php
+++ resources/views/paginas/partial/imagenes_galeria.blade.php
@@ -4,7 +4,7 @@
         <div class="row mx-0">
           @foreach($Entidad->imagenes as $Imagen)
             <a href="{{$Imagen->url_img}}" data-toggle="lightbox" data-gallery="example-gallery" class="col-4 col-lg-2 p-2 mb-2 d-flex flex-column align-items-center">
-                <img src="{{$Imagen->url_img_chica}}" class="img-fluid">
+                <img src="{{$Imagen->url_img_chica}}" class="img-cover-mini-slider">
             </a>
           @endforeach
         </div>
