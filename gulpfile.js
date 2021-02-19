741c7e085e29c52c73e43dac90f9ced91be2feda
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-02-19 11:52:41 -0300

    df

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var elixir = require('laravel-elixir');
+var elixir = require("laravel-elixir");
 
 /*
  |--------------------------------------------------------------------------
@@ -12,54 +12,45 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    
+  mix.sass("credo_scss/style.scss", "public/css/credo.css");
+  mix.sass("admin.scss", "public/css");
 
-     
-    
-     mix.sass('credo_scss/style.scss','public/css/credo.css'); 
-     mix.sass('admin.scss','public/css'); 
+  mix.scripts(
+    [
+      "Template_creative/jquery.js",
+      "Template_creative/bootstrap.bundle.js",
+      "Customs/helper_generales.js",
+      "Customs/admin_eventos.js",
+      "Plugins/Plug-lazyLoadXT.js"
+    ],
+    "public/js/admin.js"
+  );
 
-     
-
-
-    mix.scripts([
-        
-        'Template_creative/jquery.js',
-        'Template_creative/bootstrap.bundle.js',
-        'Customs/helper_generales.js',   
-        'Customs/admin_eventos.js',
-        'Plugins/Plug-lazyLoadXT.js'
-       
-
-       ],'public/js/admin.js');
-
-   
-
-     mix.scripts([
-        
-        
-        
-        'credo_js/jquery-3.3.1.min.js',
-        /*'credo_js/jquery-ui.js',*/
-       /* 'credo_js/popper.min.js',*/
-        'credo_js/bootstrap.min.js',
+  mix.scripts(
+    [
+      "credo_js/jquery-3.3.1.min.js",
+      /*'credo_js/jquery-ui.js',*/
+      /* 'credo_js/popper.min.js',*/
+      "credo_js/bootstrap.min.js",
       /*  'credo_js/owl.carousel.min.js',*/
-        'credo_js/jquery.easing.1.3.js',
-       /* 'credo_js/aos.js',
+      "credo_js/jquery.easing.1.3.js",
+      /* 'credo_js/aos.js',
         'credo_js/jquery.fancybox.min.js',*/
-        'credo_js/jquery.sticky.js',
-        'credo_js/main.js', 
-        'Plugins/Plug-lazyLoadXT.js',  
-        'Plugins/Plug-Notify.js'
+      "credo_js/jquery.sticky.js",
+      "Plugins/Plug-Notify.js",
+      "credo_js/main.js",
+      "Plugins/Plug-lazyLoadXT.js",
+      "Plugins/Plug-Notify.js"
+    ],
+    "public/js/credo.js"
+  );
 
-       
-
-       ],'public/js/credo.js');
-
-
-    elixir(function(mix) {
-            mix.version(['css/credo.css','css/admin.css' ,'js/admin.js','js/credo.js']); 
-    });
-
-    
+  elixir(function(mix) {
+    mix.version([
+      "css/credo.css",
+      "css/admin.css",
+      "js/admin.js",
+      "js/credo.js"
+    ]);
+  });
 });
--- resources/assets/js/credo_js/main.js
+++ resources/assets/js/credo_js/main.js
@@ -1,145 +1,130 @@
 jQuery(document).ready(function($) {
-
   "use strict";
 
-
   var siteMenuClone = function() {
-
-    $('.js-clone-nav').each(function() {
+    $(".js-clone-nav").each(function() {
       var $this = $(this);
-      $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+      $this
+        .clone()
+        .attr("class", "site-nav-wrap")
+        .appendTo(".site-mobile-menu-body");
     });
 
-
     setTimeout(function() {
-      
       var counter = 0;
-      $('.site-mobile-menu .has-children').each(function(){
+      $(".site-mobile-menu .has-children").each(function() {
         var $this = $(this);
-        
+
         $this.prepend('<span class="arrow-collapse collapsed">');
 
-        $this.find('.arrow-collapse').attr({
-          'data-toggle' : 'collapse',
-          'data-target' : '#collapseItem' + counter,
+        $this.find(".arrow-collapse").attr({
+          "data-toggle": "collapse",
+          "data-target": "#collapseItem" + counter
         });
 
-        $this.find('> ul').attr({
-          'class' : 'collapse',
-          'id' : 'collapseItem' + counter,
+        $this.find("> ul").attr({
+          class: "collapse",
+          id: "collapseItem" + counter
         });
 
         counter++;
-
       });
-
     }, 1000);
 
-    $('body').on('click', '.arrow-collapse', function(e) {
+    $("body").on("click", ".arrow-collapse", function(e) {
       var $this = $(this);
-      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
-        $this.removeClass('active');
+      if (
+        $this
+          .closest("li")
+          .find(".collapse")
+          .hasClass("show")
+      ) {
+        $this.removeClass("active");
       } else {
-        $this.addClass('active');
+        $this.addClass("active");
       }
-      e.preventDefault();  
-      
+      e.preventDefault();
     });
 
     $(window).resize(function() {
       var $this = $(this),
         w = $this.width();
 
-      if ( w > 768 ) {
-        if ( $('body').hasClass('offcanvas-menu') ) {
-          $('body').removeClass('offcanvas-menu');
+      if (w > 768) {
+        if ($("body").hasClass("offcanvas-menu")) {
+          $("body").removeClass("offcanvas-menu");
         }
       }
-    })
+    });
 
-    $('body').on('click', '.js-menu-toggle', function(e) {
+    $("body").on("click", ".js-menu-toggle", function(e) {
       var $this = $(this);
       e.preventDefault();
 
-      if ( $('body').hasClass('offcanvas-menu') ) {
-        $('body').removeClass('offcanvas-menu');
-        $this.removeClass('active');
+      if ($("body").hasClass("offcanvas-menu")) {
+        $("body").removeClass("offcanvas-menu");
+        $this.removeClass("active");
       } else {
-        $('body').addClass('offcanvas-menu');
-        $this.addClass('active');
+        $("body").addClass("offcanvas-menu");
+        $this.addClass("active");
       }
-    }) 
+    });
 
     // click outisde offcanvas
     $(document).mouseup(function(e) {
       var container = $(".site-mobile-menu");
       if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ( $('body').hasClass('offcanvas-menu') ) {
-          $('body').removeClass('offcanvas-menu');
+        if ($("body").hasClass("offcanvas-menu")) {
+          $("body").removeClass("offcanvas-menu");
         }
       }
     });
-  }; 
+  };
   siteMenuClone();
 
-
-
-
-
-
-
-
-  
-  
-
-
-
-  
-
-
   var siteSticky = function() {
-    $(".js-sticky-header").sticky({topSpacing:0});
+    $(".js-sticky-header").sticky({ topSpacing: 0 });
   };
   siteSticky();
 
   // navigation
   var OnePageNavigation = function() {
-    var navToggler = $('.site-menu-toggle');
-    $("body").on("click", ".main-menu li a[href^='#'], .scroll_to, .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a[href^='#']", function(e) {
-      e.preventDefault();
+    var navToggler = $(".site-menu-toggle");
+    $("body").on(
+      "click",
+      ".main-menu li a[href^='#'], .scroll_to, .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a[href^='#']",
+      function(e) {
+        e.preventDefault();
 
-      var hash = this.hash;
+        var hash = this.hash;
 
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 600, 'easeInOutExpo', function(){
-        window.location.hash = hash;
-      });
-
-    });
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top
+          },
+          600,
+          "easeInOutExpo",
+          function() {
+            window.location.hash = hash;
+          }
+        );
+      }
+    );
   };
   OnePageNavigation();
 
   var siteScroll = function() {
-
-    
-
     $(window).scroll(function() {
-
       var st = $(this).scrollTop();
 
       if (st > 100) {
-        $('.js-sticky-header').addClass('shrink');
+        $(".js-sticky-header").addClass("shrink");
       } else {
-        $('.js-sticky-header').removeClass('shrink');
+        $(".js-sticky-header").removeClass("shrink");
       }
-
-    }) 
-
+    });
   };
   siteScroll();
 
-
-
-
+  $("header, section, footer").SnapScroll();
 });
